d181281d2222a5892bd90b5e42bfd2a4a3cf3db6
Diana García <diana.eli.gg@gmail.com>
2019-12-03 13:08:19 -0600

    random

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 function changeImg(x){
-    x.src = "images/rcg_hover.jpg";
+    let r = Math.floor(Math.random() * 2);
+    console.log(r);
+    x.src = r === 0 ? "images/rcg_hover.jpg" : "images/rcg_hover_alt.jpg";
 }
 
 function returnImg(x){
