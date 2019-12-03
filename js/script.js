be9cefc71564f860ed5035efbf8f4173b9a54170
Diana García <diana.eli.gg@gmail.com>
2019-12-03 12:03:22 -0600

    added on hover main img

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
-var i = 0;
-(function(){
-    //setInterval(createElement, 1000);
-})();
-
-function createElement(){
-    console.log(++i);
-    let p = document.createElement('p');
-    p.innerHTML = 'hihihi';
-    p.className += 'test';
-    document.getElementById('container').appendChild( p );
+function changeImg(x){
+    x.src = "images/rcg_hover.jpg";
 }
 
-
-
-//createElement();
+function returnImg(x){
+    x.src = "images/rcg_pic.png";
+}
