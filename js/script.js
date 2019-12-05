77438df07e74f5e0b97b79faa2efc6c15c4ab8b5
Diana García <diana.eli.gg@gmail.com>
2019-12-04 19:13:27 -0600

    performance on main pic hover

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function changeImg(x){
     let r = Math.floor(Math.random() * 2);
     console.log(r);
-    x.src = r === 0 ? "images/rcg_hover.jpg" : "images/rcg_hover_alt.jpg";
+    x.src = r === 0 ? "images/rcg_hover.png" : "images/rcg_hover_alt.jpg";
 }
 
 function returnImg(x){
